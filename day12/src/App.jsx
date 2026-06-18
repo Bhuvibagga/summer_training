a166c1f1a24327ce9b391157b44f4aa2a67389f0
Bhuvi Bagga <bhuvibagga1221@gmail.com>
2026-06-18 16:01:18 +0530

    day12

---
+++ day12/src/App.jsx
@@ -0,0 +1,17 @@
+import Header from "./components/Header";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Header />
+
+      <main>
+        <h2>Welcome to my React App</h2>
+        
+      </main>
+    </>
+  );
+}
+
+export default App;
